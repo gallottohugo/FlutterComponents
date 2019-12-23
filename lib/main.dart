46907af153a1dd25a9ef627a4930681c7ccbcc9d
gallottohugo <gallottohugo@gmail.com>
2019-12-23 18:01:36 -0300

    Add ListView to home_temp.dart and functios to fill it

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,7 @@ class MyApp extends StatelessWidget {
 		return MaterialApp(
 	  		title: 'Components App',
 			debugShowCheckedModeBanner: false,
-	  		home: Scaffold(
-				appBar: AppBar(
-		  			title: Text('Components'),
-				),
-				body: HomePageTemp(),
-	  		),
+	  		home: HomePageTemp()
 		);
   	}
 }
--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
+
+final _options = ['uno', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 
+'ocho', 'nueve', 'diez', 'once', 'doce', 'trece', 'catorce', 'quince'];
 	@override
 	Widget build(BuildContext context) {
-		return Center(
-			child: Text('Home temp'),				
-		);
+		return Scaffold(
+      		appBar: AppBar(
+        		title: Text('Temp components'),
+      		),
+      		body: ListView(
+        		//children: _itemsCreate()
+				children: _itemsCreateShort(),
+      		),
+    	);
+	}
+
+
+	List<Widget> _itemsCreate(){
+		List<Widget> _list = new List<Widget>();
+		for (String item in _options){
+			_list..add(ListTile(title: Text(item)))
+				 ..add(Divider());
+		}
+		return _list;
+	}
+
+	List<Widget> _itemsCreateShort() {
+		return _options.map( (item) {
+			return Column(
+			  	children: <Widget>[
+			    	ListTile(
+						title: Text('$item!'),
+						subtitle: Text('Subtitle $item'),
+						leading: Icon(Icons.local_see),
+						trailing: Icon(Icons.arrow_forward_ios),
+						onTap: (){
+							
+						},
+					),
+					Divider()
+			  	],
+			);
+		}).toList();
+		
 	}
 }
