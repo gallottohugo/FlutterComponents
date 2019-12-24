aeceb14603e05a8b7edcc056b9e4fcd528aa0937
gallottohugo <gallottohugo@gmail.com>
2019-12-24 09:07:12 -0300

    Conver _loadData to asynchronous method (FUTURE)

--- lib/src/providers/menu_provider.dart
+++ lib/src/providers/menu_provider.dart
@@ -8,12 +8,23 @@ class _MenuProvider {
 		_loadData();
 	}
 
+	/* Synchronous
   	void _loadData() {
 		rootBundle.loadString('data/menu_opts.json').then((data){
 			Map dataMap = json.decode(data);
 			options = dataMap['rutas'];
 		});
 	}
+	*/ 
+
+	// Asynchronous
+  	Future<List<dynamic>> _loadData() async {
+		final response = await  rootBundle.loadString('data/menu_opts.json');
+		Map dataMap = json.decode(response);
+		options = dataMap['rutas'];
+		return options;
+	}
+	 
 }
 
 final menuProvider = new _MenuProvider();
