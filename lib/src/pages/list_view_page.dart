f381de9c7714445e8b6c8f3b13926c2f45fd011c
gallottohugo <gallottohugo@gmail.com>
2019-12-24 11:43:32 -0300

    Create avar_page and alert_page | Add Navigator to list to navigate to another page and add Navigator.pop to alert_page to go back page

--- lib/src/pages/list_view_page.dart
+++ lib/src/pages/list_view_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_components/src/pages/alert_page.dart';
 import 'package:flutter_components/src/providers/menu_provider.dart';
 import 'package:flutter_components/src/utils/icons_utils.dart';
 
@@ -14,20 +15,26 @@ class ListViewPage extends StatelessWidget {
 			future: menuProvider.loadData(),
 			builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot){
 				return ListView(
-					children: _listItemsFuture(snapshot.data),
+					children: _listItemsFuture(snapshot.data, context),
 				);
 			},
 		);
 	}
 
-	List<Widget> _listItemsFuture( List<dynamic> data ) {
+	List<Widget> _listItemsFuture( List<dynamic> data, BuildContext context ) {
 		final List<Widget> listWidget = [];
 		for (dynamic item in data){
 			final listTile = ListTile(
 				title: Text(item['texto']),
 				leading: getIcon(item['icon']),
 				trailing: Icon(Icons.arrow_forward_ios),
-				onTap: (){},
+				onTap: (){
+					final route = MaterialPageRoute(
+						builder: (context) => AlertPage()
+						
+					);
+					Navigator.push(context, route);
+				},
 			);
 			listWidget..add(listTile)
 					..add(Divider());
