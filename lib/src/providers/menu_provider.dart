fdaab7a802c232b441186a9c6ee0370600d5bf04
gallottohugo <gallottohugo@gmail.com>
2019-12-24 09:00:41 -0300

    Convert string json file to Map

--- lib/src/providers/menu_provider.dart
+++ lib/src/providers/menu_provider.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/services.dart' show rootBundle;
-
+import 'dart:convert';
 
 class _MenuProvider {
 	List<dynamic> options = [];
@@ -10,7 +10,8 @@ class _MenuProvider {
 
   	void _loadData() {
 		rootBundle.loadString('data/menu_opts.json').then((data){
-			print(data);
+			Map dataMap = json.decode(data);
+			options = dataMap['rutas'];
 		});
 	}
 }
