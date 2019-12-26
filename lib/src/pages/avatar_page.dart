ede69f5b2d0a01ee9a0c64c81b67f2a81897c477
gallottohugo <gallottohugo@gmail.com>
2019-12-26 17:50:25 -0300

    Widget - Circle Avatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,7 +6,29 @@ class AvatarPage extends StatelessWidget {
     	return Scaffold(
 			appBar: AppBar(
 				title: Text('Avatar page'),
+				actions: <Widget>[
+					Container(
+						padding: EdgeInsets.all(2),
+					  	child: CircleAvatar(
+					  		backgroundImage: NetworkImage('http://gallottohugo.com/img/team/profile.jpg'),
+					  		radius: 25,
+					  	),
+					),
+					Container(
+						margin: EdgeInsets.only(right: 10),
+					  	child: CircleAvatar(
+					  		child: Text('HG'),
+					  	),
+					), 
+				],
 			),
+			body: Center(
+				child: FadeInImage(
+					image: NetworkImage('http://gallottohugo.com/img/team/profile.jpg'),
+					placeholder: AssetImage('assets/down.gif'),
+				),
+			),
+			
 		);
   	}
 }
