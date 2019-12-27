de9fa59d803055be92579837e3814ffc655fa5af
gallottohugo <gallottohugo@gmail.com>
2019-12-27 14:36:15 -0300

    Add inputs to rorutes

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -4,6 +4,7 @@ import 'package:flutter_components/src/pages/animated_container_page.dart';
 import 'package:flutter_components/src/pages/avatar_page.dart';
 import 'package:flutter_components/src/pages/card_page.dart';
 import 'package:flutter_components/src/pages/home_page.dart';
+import 'package:flutter_components/src/pages/input_page.dart';
 
 
 
@@ -12,8 +13,9 @@ Map<String, WidgetBuilder> getApplicationRoutes(){
 		'home'   : (BuildContext context) => HomePage(),
 		'avatar' : (BuildContext context) => AvatarPage(),
 		'alert'  : (BuildContext context) => AlertPage(),
-		'card'  : (BuildContext context) =>CardPage(),
-    'animated_container': (BuildContext context) =>AnimatedContainerPage()
+		'card'  : (BuildContext context) => CardPage(),
+    'animated_container': (BuildContext context) => AnimatedContainerPage(),
+    'inputs': (BuildContext context) => InputPage(),
 	};
 }
 
