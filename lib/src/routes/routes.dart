886beac625660ea82cbdb4127ee89440d6da8d55
gallottohugo <gallottohugo@gmail.com>
2019-12-30 10:00:03 -0300

    Add list to routes

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -5,8 +5,8 @@ import 'package:flutter_components/src/pages/avatar_page.dart';
 import 'package:flutter_components/src/pages/card_page.dart';
 import 'package:flutter_components/src/pages/home_page.dart';
 import 'package:flutter_components/src/pages/input_page.dart';
+import 'package:flutter_components/src/pages/listview_builder_page.dart';
 import 'package:flutter_components/src/pages/slider_page.dart';
-
 
 
 Map<String, WidgetBuilder> getApplicationRoutes(){
@@ -14,11 +14,11 @@ Map<String, WidgetBuilder> getApplicationRoutes(){
 		'home'   : (BuildContext context) => HomePage(),
 		'avatar' : (BuildContext context) => AvatarPage(),
 		'alert'  : (BuildContext context) => AlertPage(),
-		'card'  : (BuildContext context) => CardPage(),
-    'animated_container': (BuildContext context) => AnimatedContainerPage(),
-    'inputs': (BuildContext context) => InputPage(),
-    'sliders': (BuildContext context) => SliderPage(),
-    
+		'card'   : (BuildContext context) => CardPage(),
+    	'animated_container': (BuildContext context) => AnimatedContainerPage(),
+    	'inputs' : (BuildContext context) => InputPage(),
+    	'sliders': (BuildContext context) => SliderPage(),
+		'lists'  : (BuildContext context) => ListviewBuilderPage(),
 	};
 }
 
