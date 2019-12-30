6461ea08e0cfd41e66fa7227e8ed28ce5653808a
gallottohugo <gallottohugo@gmail.com>
2019-12-30 10:36:23 -0300

    Add ScrollController

--- lib/src/pages/listview_builder_page.dart
+++ lib/src/pages/listview_builder_page.dart
@@ -7,7 +7,20 @@ class ListviewBuilderPage extends StatefulWidget {
 
 class _ListviewBuilderPageState extends State<ListviewBuilderPage> {
 
-	List<int> _listNumbers = [1,2,3,4,5,6,7,8,9];
+	List<int> _listNumbers = [];
+	int _lastItem = 0;
+	ScrollController _scrollController = new ScrollController();
+
+	@override
+ 	 void initState() {
+    	super.initState();
+		_add10images();
+		_scrollController.addListener((){
+			if (_scrollController.position.pixels == _scrollController.position.maxScrollExtent ){
+				_add10images();
+			}
+		});
+  	}
 
   	@override
   	Widget build(BuildContext context) {
@@ -21,6 +34,7 @@ class _ListviewBuilderPageState extends State<ListviewBuilderPage> {
 
 	Widget _newListView(){
 		return ListView.builder(
+			controller: _scrollController,
 			itemCount: _listNumbers.length,
 			itemBuilder: (BuildContext context, int index){
 				final _itemImg = _listNumbers[index];
@@ -32,4 +46,16 @@ class _ListviewBuilderPageState extends State<ListviewBuilderPage> {
 		);
 	}
 
+	void _add10images(){
+		for (var i = 1; i < 10; i++) {
+			_lastItem++;
+			_listNumbers.add(_lastItem);
+		}
+		setState(() {
+			
+		});
+	}
+
+
+	
 }
